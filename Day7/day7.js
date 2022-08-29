caef4500f8cee90b22e562fb34172b14c8666e4c
Paulo Barata <pbara7a@gmail.com>
2022-08-29 22:07:25 +0100

    Day 7, part 2.

--- Day7/day7.js
+++ Day7/day7.js
@@ -21,3 +21,27 @@ const minFuel = Math.min(...Object.values(fuelMap));
 for (const pos in fuelMap) {
   if (fuelMap[pos] === minFuel) console.log(pos, fuelMap[pos]);
 }
+
+// Part 2
+const positions2 = Array.from(Array(Math.max(...input)).keys());
+
+const fuelMap2 = {};
+
+positions2.forEach((x) => {
+  fuelMap2[x] = input
+    .map((crabPos, i) => {
+      const dif = Math.abs(crabPos - x);
+      let totalFuel = 0;
+      for (let i = 0; i <= dif; i++) {
+        totalFuel += i;
+      }
+      return totalFuel;
+    })
+    .reduce((acc, cur) => acc + cur);
+});
+
+const minFuel2 = Math.min(...Object.values(fuelMap2));
+
+for (const pos in fuelMap2) {
+  if (fuelMap2[pos] === minFuel2) console.log(pos, fuelMap2[pos]);
+}

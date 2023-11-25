43596d3b139b89089a72adae0192530ae50067f3
PBara7a <pbara7a@gmail.com>
2023-11-25 10:35:52 +0000

    2022 Day 4 Python

---
+++ src/2022/Day4/solution.py
@@ -0,0 +1,18 @@
+with open("input.txt") as f:
+    data = f.read()
+data = data.replace("\r\n", "\n") # Just in case I run this in Windows
+data = data.split("\n")
+
+def is_fully_contained(section_a: list, section_b: list) -> bool:
+    return (section_a[0] >= section_b[0] and section_a[1] <= section_b[1]) or (section_a[0] <= section_b[0] and section_a[1] >= section_b[1])
+
+def is_overlaping(section_a: list, section_b: list) -> bool:
+    return section_a[1] >= section_b[0] and section_b[1] >= section_a[0]
+
+pairs = [[list(map(int, range_pair.split("-"))) for range_pair in line.split(",")] for line in data]
+
+fully_contained_count = sum(is_fully_contained(pair[0], pair[1]) for pair in pairs)
+overlaping_count = sum(is_overlaping(pair[0], pair[1]) for pair in pairs)
+
+print("Part A: ", fully_contained_count)
+print("Part B: ", overlaping_count)

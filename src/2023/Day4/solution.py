7c11a2081abb702c35a4c287e42a4bd4660cbda5
PBara7a <pbara7a@gmail.com>
2023-12-04 11:14:23 +0000

    2023 Day 4 Python

---
+++ src/2023/Day4/solution.py
@@ -0,0 +1,27 @@
+with open("input.txt") as f:
+    data = f.read()
+data = data.replace("\r\n", "\n") # Just in case I run this on Windows
+data = data.split("\n")
+
+def parse_card(card: str) -> tuple:
+    numbers = card.split(": ")[1]
+    winning_numbers, card_numbers = numbers.split(" | ")
+    winning_numbers = winning_numbers.split()
+    card_numbers = card_numbers.split()
+    return { "winning_numbers": winning_numbers, "card_numbers": card_numbers, "copies": 1 }
+
+parsed_cards = [parse_card(card) for card in data]
+
+points, scratch_cards = 0, 0
+for card_id, card in enumerate(parsed_cards, start=1):
+    num_winners = len(set(card["winning_numbers"]).intersection(set(card["card_numbers"])))
+    card_score = 2 ** (num_winners - 1) if num_winners > 0 else 0
+    
+    points += card_score
+    scratch_cards += card["copies"]
+
+    for next_card in range(card_id, card_id + num_winners):
+        parsed_cards[next_card]["copies"] += card["copies"]
+
+print("Part A: ", points)
+print("Part B: ", scratch_cards)
